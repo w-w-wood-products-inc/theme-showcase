a28faa4ded3763a8a1ab3f65c8d4479b06143ab7
Jake Byrd <byrdjake126@gmail.com>
2019-06-18 11:32:01 -0500

    import spicebush theme

--- src/register_themes.js
+++ src/register_themes.js
@@ -3,11 +3,13 @@ import { ThemeManager } from '@mozaik/ui'
 import {
     nightGreen,
     sunsetTheme,
-    woodTheme
+    woodTheme,
+    spiceBush
 
 } from 'mozaik-themes-extra'
 
 ThemeManager.add(nightGreen)
 ThemeManager.add(sunsetTheme)
 ThemeManager.add(woodTheme)
+ThemeManager.add(spiceBush)
 ThemeManager.defaultTheme = woodTheme.name
